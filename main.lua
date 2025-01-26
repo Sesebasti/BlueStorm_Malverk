8ff828e9e64b99546f4980b4ad5466903c2a7dc1
Sebastián Escudero <116760721+Sesebasti@users.noreply.github.com>
2025-01-26 16:07:05 -0300

    Add files

---
+++ main.lua
@@ -0,0 +1,25 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: BlueStorm
+--- MOD_ID: malverk_bluestorm
+--- PREFIX: malverk_bluestorm
+--- MOD_AUTHOR: [eaze, SeSebasti]
+--- MOD_DESCRIPTION: Replaces the texture of Blueprint and Brainstorm. Art by eaze. Code by SeSebasti.
+--- VERSION: 1.0.0
+--- DEPENDENCIES: [malverk]
+
+AltTexture{
+    key = 'bluestorm',
+    set = 'Joker',
+    path = 'BlueStorm.png',
+    keys = {
+        'j_blueprint',
+        'j_brainstorm'
+    },
+}
+
+TexturePack{
+    key = 'bluestorm',
+    textures = {
+        'malverk_bluestorm_bluestorm'
+    }
+}
